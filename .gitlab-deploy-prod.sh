22428f3778a51993c5ce69d8bad53bf6a1b72831
Project Referral <project181219@gmail.com>
2020-06-06 20:57:28 +0000

    Add new file

---
+++ .gitlab-deploy-prod.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+
+# Get servers list:
+set — f
+# Variables from GitLab server:
+string=$PROD_DEPLOY_SERVER
+# Note: They can’t have spaces!!
+array=(${string//,/ })
+# Iterate servers for deploy and pull last commit
+# Careful with the ; https://stackoverflow.com/a/20666248/1057052
+for i in “${!array[@]}”; do
+  echo “Deploy project on server ${array[i]}”
+ssh ubuntu@${array[i]} "cd account-api && git pull origin master"
+
+done

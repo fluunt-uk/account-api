7d60a2028c256cd38d2118014215cde2186c655c
Project Referral <project181219@gmail.com>
2020-06-08 14:15:36 +0000

    Update gitlab-deploy-prod.sh

--- gitlab-deploy-prod.sh
+++ gitlab-deploy-prod.sh
@@ -8,7 +8,7 @@ string=$PROD_DEPLOY_SERVER
 array=(${string//,/ })
 # Iterate servers for deploy and pull last commit
 # Careful with the ; https://stackoverflow.com/a/20666248/1057052
-for i in '${!array[@]}'; do
+for i in "${!array[@]}"; do
   echo 'Deploy project on server ${array[i]}'
 ssh ubuntu@${array[i]} 'cd account-api && git pull origin master'
 

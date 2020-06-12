439b599022b05ca4a068b72bf4392db7f030556f
Project Referral <project181219@gmail.com>
2020-06-12 19:20:21 +0000

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"gitlab.com/projectreferral/account-api/cmd/dep"
 	"gitlab.com/projectreferral/account-api/configs"
 	"gitlab.com/projectreferral/account-api/internal/api"
@@ -13,7 +12,7 @@ import (
 
 func main() {
 
-	f, err := os.OpenFile("~"/logs/accountAPI_log.txt", os.O_WRONLY|os.O_CREATE, 0644)
+	f, err := os.OpenFile("~/logs/accountAPI_log.txt", os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}

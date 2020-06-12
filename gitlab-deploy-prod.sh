177da56ca7f1242312f4f82bbfa02fa1e426b1b9
Project Referral <project181219@gmail.com>
2020-06-12 21:51:03 +0000

    Update gitlab-deploy-prod.sh

--- gitlab-deploy-prod.sh
+++ gitlab-deploy-prod.sh
@@ -13,6 +13,6 @@ array=(${string//,/ })
 # Careful with the ; https://stackoverflow.com/a/20666248/1057052
 for i in "${!array[@]}"; do
   echo "Deploy project on server ${array[i]}"
-ssh -n ubuntu@${array[i]} "nohup go run main.go 1>hop.txt 2>hopf.txt &"
+ssh -n ubuntu@${array[i]} "cd account-api/cmd && git pull && nohup go run main.go 1>hop.txt 2>hopf.txt &"
 
 done
